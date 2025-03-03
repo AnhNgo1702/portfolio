2efcdbb850d40847bd8a7f9b04cc6cebd19ecebe
AnhNgo1702 <ngotrianh196@gmail.com>
2025-03-03 20:38:03 +0700

    Update

---
+++ data/user-data.js
@@ -0,0 +1,22 @@
+export const userData = {
+  githubUser: 'AnhNgo1702',
+  devUsername: "Ngo Tri Anh",
+  github: "https://github.com/AnhNgo1702",
+  facebook: "https://www.facebook.com/ngtr.anh1702/",
+  linkedIn: "https://www.linkedin.com/in/ngô-trí-anh-658949346/",
+  twitter: "https://x.com/008Ngo",
+  stackOverflow: "https://stackoverflow.com/users/29859857/ngô-trí-anh",
+  leetcode: "https://leetcode.com/u/AnhNgo1702/",
+  //resume: "",
+  skills: [
+    ".NET",
+    "React",
+    "NextJS",
+    "MySql",
+    "MongoDB",
+    "Postgres",
+    "Docker",
+    "AWS",
+  ],
+  timezone: "+7",
+};
