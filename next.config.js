c050ffe200a09d45127bcc80f5f6126b8f3f082c
AnhNgo1702 <ngotrianh196@gmail.com>
2025-03-03 20:48:30 +0700

    Update

--- next.config.js
+++ next.config.js
@@ -1,25 +1,11 @@
-const path = require("path");
-const isProd = process.env.NODE_ENV === "production";
-
+const path = require('path')
+ 
 module.exports = {
   sassOptions: {
-    includePaths: [path.join(__dirname, "styles")],
+    includePaths: [path.join(__dirname, 'styles')],
   },
   images: {
-    domains: [
-      "media.dev.to",
-      "avatars.githubusercontent.com",
-      "github-readme-stats.vercel.app",
-      "github-readme-streak-stats.herokuapp.com",
-      "stardev.io",
-      "github-profile-summary-cards.vercel.app",
-      "github-profile-trophy.vercel.app",
-    ],
+    domains: ['media.dev.to', 'avatars.githubusercontent.com', 'github-readme-stats.vercel.app', 'github-readme-streak-stats.herokuapp.com', 'stardev.io', 'github-profile-summary-cards.vercel.app', 'github-profile-trophy.vercel.app'],
     dangerouslyAllowSVG: true,
-    unoptimized: true,
-  },
-  basePath: isProd ? "/portfolio" : "",
-  assetPrefix: isProd ? "/portfolio/" : "",
-  output: "export", // Enables static exports
-  reactStrictMode: true,
-};
+  }
+}
