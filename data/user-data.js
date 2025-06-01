829125c82915c73cb4fb5fe6a51c09718a53b59f
AnhNgo1702 <ngotrianh196@gmail.com>
2025-06-01 15:54:07 +0700

    .

--- data/user-data.js
+++ data/user-data.js
@@ -1,13 +1,13 @@
 export const userData = {
   githubUser: 'AnhNgo1702',
-  devUsername: "AnhNgo1702",
+  devUsername: 'AnhNgo1702',
   github: 'https://github.com/AnhNgo1702',
   facebook: '#',
   linkedIn: 'https://www.linkedin.com/in/ngô-trí-anh-658949346/',
   twitter: '#',
   stackOverflow: '#',
-  leetcode: "https://leetcode.com/u/AnhNgo1702/",
-  resume: "#",
+  leetcode: 'https://leetcode.com/u/AnhNgo1702/',
+  resume: '#',
   languages: ['Javascript', 'Python', 'Java', 'C#', 'C/C++'],
   technologies: ['React', 'Redux', 'NodeJS/Express', 'Java Spring Boot', 'MySQL', 'MongoDB', 'Docker'],
   timezone: '+7'
